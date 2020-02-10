55bf4c4906f011533b61715fd690eb4d6d78ac2b
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2020-02-11 00:16:54 +0900

    :fire: Remove unnecessary tests

--- projects/lib-ngx-menu/src/lib/lib-ngx-menu-item/lib-ngx-menu-item.directive.spec.ts
+++ projects/lib-ngx-menu/src/lib/lib-ngx-menu-item/lib-ngx-menu-item.directive.spec.ts
@@ -1,8 +1,3 @@
 import { LibNgxMenuItemDirective } from './lib-ngx-menu-item.directive';
 
-describe('LibNgxMenuItemDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LibNgxMenuItemDirective();
-    expect(directive).toBeTruthy();
-  });
-});
+describe('LibNgxMenuItemDirective', () => {});
